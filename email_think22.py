34942b35f51c564afe56e2d0d2db7b3ee104a72d
AHIJLN <101846635+AHIJLN@users.noreply.github.com>
2025-03-10 19:30:06 +0800

    Update email_think22.py

--- email_think22.py
+++ email_think22.py
@@ -95,7 +95,7 @@ def get_email_body(msg):
         return ""
 
 
-def fetch_emails_in_time_range_uid(mail, start_hour=18, end_hour=22):
+def fetch_emails_in_time_range_uid(mail, start_hour=19, end_hour=22):
     """
     使用 UID 模式检索收件箱中所有邮件，
     筛选出当天（北京时间）且在 start_hour（含）到 end_hour（不含）之间的邮件，
@@ -389,7 +389,7 @@ def main():
         mail.select('INBOX')
 
         print("检查今日邮件...")
-        emails = fetch_emails_in_time_range_uid(mail, start_hour=8, end_hour=22)
+        emails = fetch_emails_in_time_range_uid(mail, start_hour=19, end_hour=22)
 
         if not emails:
             print("没有找到今日邮件")
